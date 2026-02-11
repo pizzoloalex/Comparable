1208546a017a1ee89f918122e22f916294b7c951
pizzolo.alex <alex.pizzolo@edu.iisleviponti.it>
2026-02-11 09:06:32 +0100

    Aggiunto gestione delle classi

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,20 @@
+import java.util.ArrayList;
+import java.util.Collections;
+
 public class Main {
     public static void main(String[] args) {
+//        ArrayList<Studente> studenti = new ArrayList<>();
+//        studenti.add(new Studente("Mario", "Rossi", 4.3));
+//        studenti.add(new Studente("Gigi", "Verdi", 7.4));
+//        studenti.add(new Studente("Axel", "Nguete", 8.2));
 
+//        Collections.sort(studenti);
+//        System.out.println(studenti);
+        Classe c1 = new Classe();
+        c1.aggiungiStudenti();
+        Collections.sort(c1.getStudenti());
+        for (Studente s: c1.getStudenti()){
+            System.out.println(s);
+        }
     }
 }
--- src/Studente.java
+++ src/Studente.java
@@ -11,12 +11,23 @@ public class Studente implements Comparable<Studente>{
 
 
     /**
-     * comparo gli studenti per la media
+     * compareTo gli dice al colletions come fare il sort
+     * comparo gli studenti per la media decrescente(prima riferimento al parametro)
+     * crescente (prima riferimento alla classe)
      * @param studente the object to be compared.
      * @return
      */
     @Override
     public int compareTo(Studente studente){
-        return (int) ((int)this.media - studente.media);
+        return Double.compare(this.media,studente.media);
+    }
+
+    @Override
+    public String toString() {
+        return "Studente{" +
+                "nome='" + nome + '\'' +
+                ", cognome='" + cognome + '\'' +
+                ", media=" + media +
+                '}';
     }
 }
